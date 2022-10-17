27ad80b1389d89b05c819ef40ef0dd821986af42
Beyza <112386181+Kirtan51@users.noreply.github.com>
2022-10-17 23:20:58 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace KurHesaplama
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double miktarTL, miktarUSD, miktarEUR;
+            double dolarkuru, eurokuru;
+            double toplam;
+
+            Console.Write("Dolar kurunu giriniz:");
+            dolarkuru = double.Parse(Console.ReadLine());
+
+            Console.Write("Avro kurunu giriniz:");
+            eurokuru = double.Parse(Console.ReadLine());
+
+            Console.Write("Türk Lirası(TL) miktarını giriniz:");
+            miktarTL = double.Parse(Console.ReadLine());
+
+            Console.Write("Dolar(USD) miktarını giriniz:");
+            miktarUSD = double.Parse(Console.ReadLine());
+
+            Console.Write("Euro(EUR) miktarını giriniz:");
+            miktarEUR = double.Parse(Console.ReadLine());
+
+            toplam = miktarUSD * dolarkuru + miktarEUR * eurokuru+ miktarTL;
+            Console.WriteLine("{0} kadar TL'niz var.", toplam);
+        }
+    }
+}
